e291fa40803fe571088ec187043140221088f03b
Reza <rezaulrahaat@gmail.com>
2023-06-19 19:59:11 +0600

    Amar_kaj_krsi

---
+++ addbook.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+    <title>Online Bookshop - Add Book</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            font-weight: bold;
+        }
+
+        input[type="text"],
+        select {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-bottom: 10px;
+        }
+        input[type="number"],
+        select {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+            margin-bottom: 10px;
+        }
+
+        input[type="file"] {
+            margin-top: 5px;
+        }
+
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+            border-radius: 4px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        .success-message {
+            color: #4CAF50;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <!-- Your header content here -->
+    </header>
+
+    <main>
+        <section class="add-book-section">
+            <h2>Add Book</h2>
+            <?php
+                session_start();
+                include 'connect.php';
+                
+                // Array of categories
+                $categories = array("Most Popular", "Religious", "Fiction", "Romantic", "Sci-Fi", "Children");
+                
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $title = $_POST['title'];
+                    $author = $_POST['author'];
+                    $image = $_FILES['image']['name'];
+                    $image_temp = $_FILES['image']['tmp_name'];
+                    $category = $_POST['category'];
+                    $price = $_POST['price'];
+                    $featured = isset($_POST['featured']) ? 1 : 0;
+
+                    $target_directory = 'book_images/';
+                    $target_file = $target_directory . $image;
+
+                    // Move the uploaded image to the target directory
+                    move_uploaded_file($image_temp, $target_file);
+
+                    $query = "INSERT INTO books (title, author, image, category, featured) VALUES ('$title', '$author', '$target_file', '$category', $featured)";
+                    mysqli_query($conn, $query);
+
+                    // Redirect back to the index page or display a success message
+                    header("Location: index.php");
+                    exit();
+                }
+            ?>
+            
+            <form action="addbook.php" method="post" enctype="multipart/form-data">
+                <label for="title">Title:</label>
+                <input type="text" id="title" name="title" required>
+
+                <label for="author">Author:</label>
+                <input type="text" id="author" name="author" required>
+
+                <label for="category">Category:</label>
+                <select id="category" name="category" required>
+                    <?php
+                        // Loop through categories and create options
+                        foreach ($categories as $category) {
+                            echo "<option value='$category'>$category</option>";
+                        }
+                    ?>
+                </select>
+
+                <label for="image">Image:</label>
+                <input type="file" id="image" name="image" accept="image/*" required>
+                <br><br>
+                <label for="price">Price:</label>
+                <input type="number" id="price" name="price" required>
+
+                <label for="featured">Featured:</label>
+                <input type="checkbox" id="featured" name="featured">
+                        <br><br>
+                <input type="submit" value="Add Book">
+            </form>
+            <?php
+                mysqli_close($conn);
+            ?>
+            <div id="success-message" class="success-message" style="display: none;">Book added successfully!</div>
+        </section>
+
+        <!-- Other sections and content of your website -->
+    </main>
+
+    <footer>
+        <!-- Your footer content here -->
+    </footer>
+    <script>
+        // JavaScript for displaying success message after form submission
+        document.getElementById("add-book-form").addEventListener("submit", function(event) {
+            event.preventDefault(); // Prevent form submission
+            
+            // Show success message
+            var successMessage = document.getElementById("success-message");
+            successMessage.style.display = "block";
+
+            // Reset form
+            this.reset();
+        });
+    </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -21,7 +21,17 @@
     <style>
        /* Add this CSS code to your style.css file */
 
-.user-box {
+       section .main .main_tag .main_btn {
+    background: #08a192;
+    padding: 10px 20px;
+    position: relative;
+    top: 150px;
+    left: 25px;
+    color: #fff;
+    text-decoration: none;
+    z-index: 1;
+  }
+       .user-box {
   position: fixed;
   background-color: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
@@ -32,6 +42,67 @@
 
 .user-box p {
   margin: 5px 0;
+}
+/* Best Books Section */
+.best-books-section {
+  margin-top: 40px;
+}
+
+.category-books {
+  margin-bottom: 30px;
+}
+
+.category-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.best-books-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 20px;
+}
+
+.book-item {
+  width: 250px;
+  background-color: #f5f5f5;
+  padding: 20px;
+  text-align: center;
+}
+
+.book-item img {
+  max-width: 100%;
+  height: auto;
+}
+
+.book-title {
+  font-size: 18px;
+  margin: 10px 0;
+}
+
+.author-name {
+  font-size: 14px;
+  color: #777;
+}
+
+.see-more-button {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.see-more-button a {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 4px;
+}
+
+.see-more-button a:hover {
+  background-color: #0069d9;
 }
 
 
@@ -128,61 +199,65 @@ footer .end span{
   margin-left: 10px;
 }
 
+
     </style>
 </head>
 
 <body>
-    <header class="header">
+<header class="header">
 
-        <div class="header-1">
+<div class="header-1">
 
-            <a href="#" class="logo"> <i class="fas fa-book"></i> Story keeper </a>
+    <a href="#" class="logo"> <i class="fas fa-book"></i> Story keeper </a>
 
-            <form action="" class="search-form">
-                <input type="search" name="" placeholder="search here..." id="search-box">
-                <label for="search-box" class="fas fa-search"></label>
-            </form>
+    <form action="" class="search-form">
+        <input type="search" name="" placeholder="search here..." id="search-box">
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
 
-            <div class="icons">
-                <div id="search-btn" class="fas fa-search"></div>
-                <a href="#" class="fas fa-heart"></a>
-                <a href="#" class="fas fa-shopping-cart"></a>
-                <?php 
-            if (isset($_SESSION['email'])) {
-                // User is logged in
-                echo '<div id = "user-btn" class="fas fa-user"></div>';
-                echo '<a href="logout.php">Log out</a>';
-            } 
-                       
-            else {
-                // User is not logged in
-                echo '<a href="loginin.php">Log in</a>';
-            }
-            ?>
-            <div class="user-box">          
-                <p>Name: <span><?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname']; ?></span></p>
-                <p>email : <span><?php echo $_SESSION['email']; ?></span></p>
-                <p>phone number : <span><?php echo $_SESSION['phone_number']; ?></span></p>
-            </div>
+    <div class="icons">
+        <div id="search-btn" class="fas fa-search"></div>
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <?php 
+        
+    if (isset($_SESSION['email'])) {
+        echo '<div id="user-btn" class="fas fa-user"></div>';
+        if ($_SESSION['email'] === 'a@a.com') {
+            // Specific email matched, show "Add Book" option
+            echo '<a href="addbook.php">Add Book</a>';
+        }
+        echo '<a href="logout.php">Log out</a>';
+    }
+    else {
+        // User is not logged in
+        echo '<a href="loginin.php">Log in</a>';
+    }
+    ?>
+    <div class="user-box">          
+        <p>Name: <span><?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname']; ?></span></p>
+        <p>email : <span><?php echo $_SESSION['email']; ?></span></p>
+        <p>phone number : <span><?php echo $_SESSION['phone_number']; ?></span></p>
+    </div>
 
-                
+        
 
-            </div>
+    </div>
 
-        </div>
+</div>
 
-        <div class="header-2">
-            <nav class="navbar">
-                <a href="#banner-section">home</a>
-                <a href="#catagoris">catagoris</a>
-                <a href="#featured">featured</a>
-                <a href="#arrivals">arrivals</a>
+<div class="header-2">
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#catagoris">catagoris</a>
+        <a href="#featured">featured</a>
+        <a href="#arrivals">arrivals</a>
+        
+        <a href="#blogs">blogs</a>
+    </nav>
+</div>
 
-                <a href="#blogs">blogs</a>
-            </nav>
-        </div>
-
-    </header>
+</header>
     <footer class="footer">
         <nav class="bottom-navbar">
             <a href="#banner-section"><i class="fas fa-home"></i> Home</a>
@@ -219,8 +294,6 @@ footer .end span{
 
 
     <!--catagori suru korlam-->
-    
-
     <section id="categories">
         <div class="container">
             <h2>Categories</h2>
@@ -292,45 +365,70 @@ footer .end span{
     </section>
 
 
+    <section class="featured-books-section">
+            <h2>Featured Books</h2>
+            <div class="books-container">
+                <?php
+                    include 'connect.php';
 
+                    $query = "SELECT * FROM books WHERE featured = 1";
+                    $result = mysqli_query($conn, $query);
 
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $bookId = $row['id'];
+                        $title = $row['title'];
+                        $author = $row['author'];
+                        $image = $row['image'];
 
+                        echo '<div class="book-card">';
+                        echo '<img src="' . $image . '" alt="' . $title . '">';
+                        echo '<h3 class="book-title">' . $title . '</h3>';
+                        echo '<p class="author-name">By: ' . $author . '</p>';
+                        echo '<form action="addtocart.php" method="post">';
+                        echo '<input type="hidden" name="book_id" value="' . $bookId . '">';
+                        echo '<button type="submit" class="add-to-cart" name="add_to_cart">Add to Cart</button>';
+                        echo '</form>';
+                        echo '</div>';
+                    }
 
+                    mysqli_close($conn);
+                ?>
+            </div>
+        </section>
+        <section class="best-books-section">
+    <?php
+    include 'connect.php';
 
+    $categories = array("Most Popular", "Religious", "Fiction", "Romantic", "Sci-Fi", "Children"); // Example categories
 
+    foreach ($categories as $category) {
+        echo '<div class="category-books">';
+        echo '<h2 class="category-title">' . $category . '</h2>';
 
+        $query = "SELECT * FROM books WHERE category = '$category' LIMIT 6"; // Retrieve 5 books per category
+        $result = mysqli_query($conn, $query);
 
+        echo '<div class="best-books-grid">';
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div class="book-item">';
+            echo '<img src="' . $row['image'] . '" alt="' . $row['title'] . '">';
+            echo '<h3 class="book-title">' . $row['title'] . '</h3>';
+            echo '<p class="author-name">By ' . $row['author'] . '</p>';
+            echo '<h3 class="book-price">Price: ' . $row['price'] . '$</h3>';
+            echo '<button><a href="addtocart.php?id=' . $row['id'] . '" class="add-to-cart-button">Add to Cart</a></button>';
+            echo '</div>';
+        }
+        echo '</div>'; // Close best-books-grid
 
+        echo '<div class="see-more-button">';
+        echo '<a href="morebooks.php?category=' . $category . '">See More</a>';
+        echo '</div>';
 
+        echo '</div>'; // Close category-books
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    mysqli_close($conn);
+    ?>
 <!--Fotter-->
 
 
@@ -390,7 +488,7 @@ footer .end span{
         </div>
 
         <p class="end">Design By<span><i class="fa-solid fa-face-grin"></i> Sajjad and Reza</span></p>
-
+  </section>
     </footer>
 </section>
 
